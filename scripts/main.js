1b665c19ab261b52499c099f518d5ff5442acb3d
ssanekmix <sashaprokopev577@gmail.com>
2025-05-09 23:16:47 +0300

    News, Blog and Podcasts pages have completed

--- scripts/main.js
+++ scripts/main.js
@@ -1,7 +1,15 @@
 import Header from './Header.js'
 import TabsCollection from './Tabs.js'
 import Like from './Like.js'
+import VideoPlayerCollection from './VideoPlayer.js'
+import ExpandableContentCollection from './ExpandableContent.js'
+import defineScrollBarWidthCSSVar from './utils/defineScrollBarWidthCSSVar.js'
+import pxToRem from './utils/pxToRem.js'
 
 new Header()
 new TabsCollection()
 new Like()
+new VideoPlayerCollection()
+new ExpandableContentCollection()
+
+defineScrollBarWidthCSSVar()
