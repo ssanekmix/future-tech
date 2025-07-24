56a4e1a58ee0579d0e3419595cfea521ebe8c450
ssanekmix <sashaprokopev577@gmail.com>
2025-07-24 13:33:49 +0400

    проект завершен

--- scripts/main.js
+++ scripts/main.js
@@ -5,11 +5,16 @@ import VideoPlayerCollection from './VideoPlayer.js'
 import ExpandableContentCollection from './ExpandableContent.js'
 import defineScrollBarWidthCSSVar from './utils/defineScrollBarWidthCSSVar.js'
 import pxToRem from './utils/pxToRem.js'
+import InputMaskCollection from './InputMask.js'
+import SelectCollection from './Select.js'
 
 new Header()
 new TabsCollection()
 new Like()
 new VideoPlayerCollection()
 new ExpandableContentCollection()
+new InputMaskCollection()
+new SelectCollection()
 
 defineScrollBarWidthCSSVar()
+
